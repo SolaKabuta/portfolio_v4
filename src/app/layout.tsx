268ba8d42adb1bb701e562291485dd438fa35656
Sola <solakabuta@gmail.com>
2024-06-18 06:03:26 +0200

    fix: added next config img fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Sola Kabuta",
+  title: "Sola Kabuta Portfolio",
   description: "Junior front-end developer portfolio",
 };
 
