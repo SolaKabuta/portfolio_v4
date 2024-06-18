c1391abb67475df30ffb90144f1f1c4af353c641
Sola <solakabuta@gmail.com>
2024-06-18 05:33:32 +0200

    fix: bad page export

--- src/app/more/playground/page.tsx
+++ src/app/more/playground/page.tsx
@@ -1,9 +1,15 @@
+import {Header} from "@/components/Header";
 
-export default function () {
-    return (
-        <section>
-            <p>Coming soon </p>
+export default function MorePlayground2 () {
+    return <div>
+        <Header/>
+        <p className={'absolute z-50 text-7xl backdrop-blur font-black grid place-items-center left-0 right-0 h-screen transition hover:-translate-x-6 ease-in-out duration-700'}>Coming soon</p>
+        <section className={'py-56'}>
+            <div className={'flex justify-between'}>
+                <div className={'box'}></div>
+                <div className={'box'}></div>
+                <div className={'box'}></div>
+            </div>
         </section>
-    );
-};
-
+    </div>
+}
