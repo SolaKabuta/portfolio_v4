865a90cd3fae4c673330d18a1a9acf5d58daff0a
NalessoAxel <axelnalesso@gmail.com>
2024-06-20 12:54:33 +0200

    Fix: add type for work data

--- src/app/work/[slug]/page.tsx
+++ src/app/work/[slug]/page.tsx
@@ -1,29 +1,32 @@
 import fs from "fs";
 import path from "path";
+import { Work } from "../../../types/workTypes";
 
 // Function to read data from data.json
-const getData = () => {
+const getData = (): Work[] => {
   const filePath = path.join(process.cwd(), "data", "works.json");
   const jsonData = fs.readFileSync(filePath, "utf-8");
-
   return JSON.parse(jsonData);
 };
 
 // Generate static paths based on data
-export async function generateStaticParams() {
-  const data = getData();
-
-  return data.map((item: any) => ({
-    slug: item.slug,
+export async function generateStaticParams(): Promise<
+  { params: { slug: string } }[]
+> {
+  const data: Work[] = getData();
+  return data.map((item: Work) => ({
+    params: {
+      slug: item.slug,
+    },
   }));
 }
 
-const getItemData = (slug: any) => {
-  const data = getData();
-  return data.find((item: any) => item.slug === slug);
+const getItemData = (slug: string): Work | undefined => {
+  const data: Work[] = getData();
+  return data.find((item: Work) => item.slug === slug);
 };
 
-const Item = ({ params }: any) => {
+const Item = ({ params }: { params: { slug: string } }) => {
   const item = getItemData(params.slug);
 
   if (!item) {
---
+++ src/types/workTypes.ts
@@ -0,0 +1,7 @@
+export type Work = {
+  title: string;
+  description: string;
+  image: string;
+  url: string;
+  slug: string;
+};
