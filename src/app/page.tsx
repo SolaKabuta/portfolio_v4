3152b04a62a6646eb9f2006e8dbacbf88286c5ee
Sola <solakabuta@gmail.com>
2024-08-28 00:07:05 +0200

    refactor: synthaxe correction/removed unused components in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import { Header } from "@/components/Header";
 import { Hero } from "@/components/Hero";
 import { Work } from "@/components/Work";
 import { About } from "@/components/About";
