e1f9f4a4d5e05400829449b38dbe891e3ff87c28
Sola <solakabuta@gmail.com>
2024-09-02 16:58:01 +0200

    refactor: projects added in menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -20,34 +20,24 @@ const Navbar = ({ className }: { className?: string }) => {
                     {/*        height={40}*/}
                     {/*        alt={'/'}/>*/}
                     {/*</div>*/}
-                    <MenuItem setActive={setActive} active={active} item="Work">
-                        <div className="  text-sm grid grid-cols-2 gap-10 p-4">
-                            <ProductItem
-                                title="Algochurn"
-                                href="https://algochurn.com"
-                                src="https://assets.aceternity.com/demos/algochurn.webp"
-                                description="Prepare for tech interviews like never before."
-                            />
-                            <ProductItem
-                                title="Tailwind Master Kit"
-                                href="https://tailwindmasterkit.com"
-                                src="https://assets.aceternity.com/demos/tailwindmasterkit.webp"
-                                description="Production ready Tailwind css components for your next project"
-                            />
-                            <ProductItem
-                                title="Moonbeam"
-                                href="https://gomoonbeam.com"
-                                src="https://assets.aceternity.com/demos/Screenshot+2024-02-21+at+11.51.31%E2%80%AFPM.png"
-                                description="Never write from scratch again. Go from idea to blog in minutes."
-                            />
-                            <ProductItem
-                                title="Rogue"
-                                href="https://userogue.com"
-                                src="https://assets.aceternity.com/demos/Screenshot+2024-02-21+at+11.47.07%E2%80%AFPM.png"
-                                description="Respond to government RFPs, RFIs and RFQs 10x faster using AI"
-                            />
-                        </div>
-                    </MenuItem>
+                    <a href="#work">
+                        <MenuItem setActive={setActive} active={active} item="Work">
+                            <div className="group text-sm grid grid-cols-2 gap-10 p-4">
+                                <ProductItem
+                                    title="Cabinet Coppens"
+                                    href="https://cabinetcoppens.be/"
+                                    src="assets/thumbnails/coppens_imac.png"
+                                    description="Mastering accounting for balanced finances"
+                                />
+                                <ProductItem
+                                    title="MARTINE Henderyckx"
+                                    href="https://martine.mobilstudio.be/"
+                                    src="assets/thumbnails/martine_thumbnail.png"
+                                    description="The art of guidance for living in harmony with oneself"
+                                />
+                            </div>
+                        </MenuItem>
+                    </a>
 
                     <MenuItem setActive={setActive} active={active} item="Contact">
                         <div className="flex flex-col space-y-4 text-sm">
