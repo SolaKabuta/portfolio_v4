d23a8757c4b5cf7fbc2059142de8124a447831f6
Sola <solakabuta@gmail.com>
2024-09-02 05:32:53 +0200

    style: footer made fully reponsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -11,51 +11,60 @@ export const Footer = () => {
     return (
         <footer className={'bg-black w-full h-max text-white'}>
             <section>
-                <div className={'flex justify-between pb-10'}>
-                    <div>
+                <div className={'flex justify-between max-md:grid pb-10'}>
+                    <div className={'max-md:hidden'}>
                         <p >Sola Kabuta</p>
                         <a className={'transition duration-300 hover:text-main ease-in-out'} href="">solakabuta@gmail.com</a>
                         <p>Brussels, Belgium</p>
                     </div>
                     <div>
-                        <Image className={'invert'} src={'/assets/icons/logo_original.svg'} alt={'Sola Kabuta logo'} width={50} height={50}/>
+                        <a href="/"><Image className={'invert max-md:pb-3'} src={'/assets/icons/logo_original.svg'} alt={'Sola Kabuta logo'} width={50} height={50}/></a>
                     </div>
                     <div>
                         <ul className={''}>
-                        <li className={style.links}>
-                            <a
-                                className={style.icons}
-                                href="https://www.linkedin.com/in/sola-kabuta/"
-                                target={'_blank'}>
-                                Linkedin <ArrowTopRightIcon/>
-                            </a>
-                        </li>
-                        <li className={style.links}>
-                            <a
-                                className={style.icons}
-                                href="https://github.com/SolaKabuta"
-                                target={'_blank'}>
-                                github <ArrowTopRightIcon/>
-                            </a>
-                        </li>
-                        <li className={style.links}>
-                            <a
-                                className={style.icons}
-                                href="/assets/images/CV_Dev.pdf"
-                                target={'_blank'}>
-                                CV <ArrowTopRightIcon/>
-                            </a>
-                        </li>
-                    </ul>
+                            <li className={'md:hidden'}>
+                                <a
+                                    className={style.icons}
+                                    href="mailto:solakabuta@gmail.com"
+                                    target={'_blank'}>
+                                    Mail <ArrowTopRightIcon/>
+                                </a>
+                            </li>
+                            <li className={style.links}>
+                                <a
+                                    className={style.icons}
+                                    href="https://www.linkedin.com/in/sola-kabuta/"
+                                    target={'_blank'}>
+                                    Linkedin <ArrowTopRightIcon/>
+                                </a>
+                            </li>
+                            <li className={style.links}>
+                                <a
+                                    className={style.icons}
+                                    href="https://github.com/SolaKabuta"
+                                    target={'_blank'}>
+                                    github <ArrowTopRightIcon/>
+                                </a>
+                            </li>
+                            <li className={style.links}>
+                                <a
+                                    className={style.icons}
+                                    href="/assets/images/CV_Dev.pdf"
+                                    target={'_blank'}>
+                                    CV <ArrowTopRightIcon/>
+                                </a>
+                            </li>
+                        </ul>
                     </div>
                 </div>
                 <h5 className={'flex justify-between font-black max-md:text-4xl text-12xl'}>
-                     <span>C</span><span>o</span><span>n</span><span>n</span><span>e</span><span>c</span><span>t</span></h5>
+                    <span>C</span><span>o</span><span>n</span><span>n</span><span>e</span><span>c</span><span>t</span>
+                </h5>
                 {/* Copyrights */}
                 <div className={'flex justify-between items-center text-main_color py-5 text-xs bg-black'}>
                     <a href="#">© Copyright - sola kabuta - 2024</a>
-                    <p className={'max-lg:hidden'}>LAST UPDATE august 2024</p>
-                    <p className={'flex gap-2'}>made with love <HeartFilledIcon/> </p>
+                    <p className={'max-lg:hidden max-md:hidden'}>LAST UPDATE august 2024</p>
+                    <p className={'flex gap-2 max-md:hidden'}>made with love <HeartFilledIcon/> </p>
                 </div>
             </section>
         </footer>
