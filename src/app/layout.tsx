8da1a59494f6006a23f8bdb6b48824f92a62822b
Sola <solakabuta@gmail.com>
2024-09-05 07:58:07 +0200

    feat: vercel speed insights added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import type { Metadata } from "next";
 import { Hanken_Grotesk, Anek_Gujarati } from "next/font/google";
 import "./globals.css";
