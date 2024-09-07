727bbd2080c84e6e60827e8c74e7c18b74eb78be
Sola <solakabuta@gmail.com>
2024-09-07 14:45:00 +0200

    style: contact page style improved

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -14,6 +14,10 @@ export const Contact = () => {
                     default_velocity={5}
                     className="font-display text-center max-md:text-4xl text-7xl font-black tracking-[-0.02em] text-main drop-shadow-sm dark:text-white md:text-7xl md:leading-[5rem]"
                 />
+                <div
+                    className="absolute z-50 inset-y-0 left-0 w-1/3 bg-gradient-to-r from-second dark:from-background"></div>
+                <div
+                    className="absolute z-50 inset-y-0 right-0 w-1/3 bg-gradient-to-l from-second dark:from-background"></div>
             </div>
         </section>
     );
